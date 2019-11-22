80a3d899d904a6a723456f6872c80b71bcccd5b4
user <user@DESKTOP-F0868HI>
2019-11-22 18:07:32 +0530

    code commited

--- ChangeLog/src/main/java/com/fintechlabs/auditlogs/repository/AuditTrailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fintechlabs.auditlogs.repository;
-
-import com.fintechlabs.auditlogs.model.AuditTrail;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-;
-
-public interface AuditTrailRepository extends JpaRepository<AuditTrail, Long> {
-}
